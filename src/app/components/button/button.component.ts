b2cbac7c3e5c9abc1ab83f0d9cb082bdf22ddcf8
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-29 08:52:09 -0300

    configuracao do navebar e do button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-
+  @Input() color: string = 'dark';
+  @Input() size: string = '';
+  @Input() disabled: boolean = false;
+  @Input() label: string = '';
 }
