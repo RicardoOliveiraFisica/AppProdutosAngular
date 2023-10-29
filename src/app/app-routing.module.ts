86af704378fd5724ed67cd0f35d92ed66010ed00
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-28 21:30:13 -0300

    configuracao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { ProductsComponent } from './pages/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/create', component: RegisterComponent },
+  { path: 'products/edit/:id', component: EditComponent },
+  { path: "", redirectTo: "home", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
